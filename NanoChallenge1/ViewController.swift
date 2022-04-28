31f20870857d9935f91cf1c84048c6cfa86cbc1b
Zacky Ilahi Azmi <zackyilahi.azmi@gmail.com>
2022-04-28 13:46:22 +0700

    Initial Commit

---
+++ NanoChallenge1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NanoChallenge1
+//
+//  Created by Zacky Ilahi Azmi on 28/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
