445131c5948c845c38c3c01b8dbecdab94bc41ba
Zacky Ilahi Azmi <zackyilahi.azmi@gmail.com>
2022-05-09 11:32:49 +0700

    photopicker update

--- NanoChallenge1/AddPhotoViewController.swift
+++ NanoChallenge1/AddPhotoViewController.swift
@@ -5,9 +5,11 @@
 //  Created by Zacky Ilahi Azmi on 29/04/22.
 //
 
+import Photos
+import PhotosUI
 import UIKit
 
-class AddPhotoViewController: UIViewController {
+class AddPhotoViewController: UIViewController, PHPickerViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,4 +21,26 @@ class AddPhotoViewController: UIViewController {
         self.dismiss(animated: true)
     }
     
+    
+    @IBAction func addItem(_ sender: Any) {
+        var config = PHPickerConfiguration(photoLibrary: .shared())
+        config.selectionLimit = 1
+        config.filter = .images
+        let vc = PHPickerViewController(configuration: config)
+        vc.delegate = self
+        present(vc, animated: true)
+    }
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        results.forEach { result in
+            result.itemProvider.loadObject(ofClass: UIImage.self) { reading, error in
+                guard let image = reading as? UIImage, error == nil else {
+                    return
+                }
+                print(image)
+            }
+        }
+    }
 }
